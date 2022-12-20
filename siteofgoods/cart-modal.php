aca67128d9ec297922a6533d1dda600dc5fb8738
Матаков Михаил РИС-21-1Б <92533785+MihailMatakov@users.noreply.github.com>
2022-12-20 14:23:27 +0500

    Вёрстка окна корзины

---
+++ siteofgoods/cart-modal.php
@@ -0,0 +1,18 @@
+            <div class="modal-body">           
+                <table class="table">
+                    <thead>
+                    <tr>
+                        <th scope="col">Image</th>
+                        <th scope="col">Title</th>
+                        <th scope="col">Price</th>
+                        <th scope="col">Qty</th>
+                    </tr>
+                    </thead>
+ 
+                </table>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Закрыть</button>
+                <button type="button" class="btn btn-primary">Оформить заказ</button>
+                <button type="button" class="btn btn-danger" id = "clear-cart">Очистить корзину</button>
+            </div>
