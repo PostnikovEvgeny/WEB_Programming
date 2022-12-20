9755ced5413e935b955766fed85c08f0ee1f233a
Постников Евгений Рис-21-1б <92161003+PostnikovEvgeny@users.noreply.github.com>
2022-12-20 14:36:54 +0500

    Добавление товара в корзину

--- siteofgoods/js/main.js
+++ siteofgoods/js/main.js
@@ -8,4 +8,30 @@ $(function() {
 		let cartQty = $('#modal-cart-qty').text() ? $('#modal-cart-qty').text() : 0;
 		$('.mini-cart-qty').text(cartQty);
 	}
+
+$('.add-to-cart').on('click', function (e) {
+        e.preventDefault();
+        let id = $(this).data('id');
+
+        $.ajax({
+            url: 'cart.php',
+            type: 'GET',
+            data: {cart: 'add', id: id},
+            dataType: 'json',
+            success: function (res) {
+                if (res.code == 'ok') {
+                    showCart(res.answer);
+                } else {
+                    alert(res.answer);
+                }
+            },
+            error: function () {
+                alert('Error');
+            }
+        });
+    });
+
+
+
+
 });
