db4a2ff337c932c3e98e1a7bf97002b69e384415
YuriyKorolyov <92149434+YuriyKorolyov@users.noreply.github.com>
2022-12-20 13:56:05 +0500

    Страница регистрации готова. Добавлен выход

--- siteofgoods/login.php
+++ siteofgoods/login.php
@@ -29,3 +29,27 @@ if ( isset($data['do_login']) )
  
 }
 ?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Авторизация</title>
+    <link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
+<body>
+
+    <div class="login-page">
+  <div class="form">
+    <form class="login-form" action="login.php" method="POST">
+      <input type="text" placeholder="Логин" name="login" value="<?php echo @$data['login']; ?>"/>
+      <input type="password" placeholder="Пароль" name="password" value="<?php echo @$data['password']; ?>"/>
+      <button type="submit" name="do_login">Войти</button>
+        </form>
+  </div>
+</div>
+
+</body>
+</html>
